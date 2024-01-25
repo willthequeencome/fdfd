8e3987f486c5b3a534627acb149f87f4ea3b779b
alex <89742348+sauc23@users.noreply.github.com>
2024-01-25 13:50:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const Fastify = require("fastify");
 const server = Fastify();
 
-server.register(require('@fastify/cors'), {
-  // Enable CORS for all origins except localhost
-  origin: (origin, cb) => {
-    if (/^localhost$/m.test(origin)) {
-      // Do not include CORS headers for requests from localhost
-      cb(null, false);
-    } else {
-      // Enable CORS for other origins
-      cb(null, true);
-    }
-  }
-});
 
 server.register(require("@fastify/http-proxy"), {
   upstream: "https://talkai.info",
