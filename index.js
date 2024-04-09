c5a37b6b7b9528186061009cf1b120318468002f
willthequeencome <161780156+willthequeencome@users.noreply.github.com>
2024-04-09 13:32:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 
-const target = 'https://thin-starfish-17.telebit.io';
+const target = 'https://45-143-220-78.tvfree.to/';
 
 // Create the proxy middleware
 const mathProxy = createProxyMiddleware({
   target,
-  changeOrigin: true, // Needed for virtual hosted sites
+  changeOrigin: true,
   logLevel: 'debug',
+  onProxyRes: function(proxyRes, req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+  }
 });
 
 // Use the proxy middleware for all requests
